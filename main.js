569a6154091e99be97f178e25e52389b24a657a3
Tahir Y. Dibirov <tahiryd@code.local>
2022-12-12 06:54:31 -0800

    INIT

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
 const display = document.querySelector('.display')
-// const one = document.querySelector('.one')
-// const two = document.querySelector('.two')
-// const three = document.querySelector('.three')
-// const four = document.querySelector('.four')
-// const five = document.querySelector('.five')
-// const six = document.querySelector('.six')
-// const seven = document.querySelector('.seven')
-// const eight = document.querySelector('.eight')
-// const nine = document.querySelector('.nine')
-// const zero = document.querySelector('.zero')
 const equals = document.querySelector('.equals')
 const clear = document.querySelector('.clear')
 const operations = document.querySelectorAll(' .plus, .minus, .divide, .multiply')
@@ -17,9 +7,7 @@ operations.forEach(operation =>{
     operation.addEventListener('click', ()=>{
         console.log(operation)
         if(display.textContent[display.textContent.length-1] === operation.getAttribute('data')) null
-        else{ 
-            
-            display.innerHTML+=operation.getAttribute('data') }
+        else display.innerHTML+=operation.getAttribute('data') 
     })
 })
 
@@ -29,7 +17,7 @@ const buttons = document.querySelectorAll('.button:not(.clear, .equals, .plus, .
 buttons.forEach(button =>{
     button.addEventListener('click', ()=>{
         display.innerHTML+=button.getAttribute('data')
-       console.log(display.textContent[display.textContent.length-2]===button.getAttribute('data'))
+      
     })
 })
 
