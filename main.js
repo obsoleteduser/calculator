4bd21c06987353831a632ea24236f1204951e592
Tahir <dibirovtahir@gmail.com>
2022-12-12 20:42:26 +0400

    Done

--- main.js
+++ main.js
@@ -3,36 +3,36 @@ const equals = document.querySelector('.equals')
 const clear = document.querySelector('.clear')
 const operations = document.querySelectorAll(' .plus, .minus, .divide, .multiply')
 console.log(operations)
-operations.forEach(operation =>{
-    operation.addEventListener('click', ()=>{
+operations.forEach(operation => {
+    operation.addEventListener('click', () => {
         console.log(operation)
-        if(display.textContent[display.textContent.length-1] === operation.getAttribute('data')) null
-        else display.innerHTML+=operation.getAttribute('data') 
+        if (display.textContent[display.textContent.length - 1] === operation.getAttribute('data')) null
+        else display.innerHTML += operation.getAttribute('data')
     })
 })
 
 
 const buttons = document.querySelectorAll('.button:not(.clear, .equals, .plus, .minus, .divide, .multiply)')
 
-buttons.forEach(button =>{
-    button.addEventListener('click', ()=>{
-        display.innerHTML+=button.getAttribute('data')
-        display.textContent > 11 ? display.innerHTML = display.textContent.substring(0,11) : null
-      
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        display.innerHTML += button.getAttribute('data')
+        display.textContent > 11 ? display.innerHTML = display.textContent.substring(0, 11) : null
+
     })
 })
 
-equals.addEventListener('click', ()=>{
+equals.addEventListener('click', () => {
     console.log(display.textContent)
-    try{
-    display.innerHTML = `${eval(display.textContent)}`
-    display.textContent > 11 ? display.innerHTML = display.textContent.substring(0,11) : null
-    } catch(e){display.innerHTML=`WTF!?`}
+    try {
+        display.innerHTML = `${eval(display.textContent)}`
+        display.textContent > 11 ? display.innerHTML = display.textContent.substring(0, 11) : null
+    } catch (e) { display.innerHTML = `WTF!?` }
 })
 
 
 
-clear.addEventListener('click', ()=>{
+clear.addEventListener('click', () => {
     display.innerHTML = ''
 })
 
