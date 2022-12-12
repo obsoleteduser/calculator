736f4a8816241c4640716860633e8613399f5e6e
Tahir Y. Dibirov <tahiryd@code.local>
2022-12-12 06:33:48 -0800

    INIT

--- main.js
+++ main.js
@@ -11,19 +11,33 @@ const display = document.querySelector('.display')
 // const zero = document.querySelector('.zero')
 const equals = document.querySelector('.equals')
 const clear = document.querySelector('.clear')
+const operations = document.querySelectorAll(' .plus, .minus, .divide, .multiply')
+console.log(operations)
+operations.forEach(operation =>{
+    operation.addEventListener('click', ()=>{
+        console.log(operation)
+        if(display.textContent[display.textContent.length-1] === operation.getAttribute('data')) null
+        else{ 
+            
+            display.innerHTML+=operation.getAttribute('data') }
+    })
+})
 
-const buttons = document.querySelectorAll('.button:not(.clear, .equals)')
+
+const buttons = document.querySelectorAll('.button:not(.clear, .equals, .plus, .minus, .divide, .multiply)')
 
 buttons.forEach(button =>{
     button.addEventListener('click', ()=>{
         display.innerHTML+=button.getAttribute('data')
-       
+       console.log(display.textContent[display.textContent.length-2]===button.getAttribute('data'))
     })
 })
 
 equals.addEventListener('click', ()=>{
     console.log(display.textContent)
+    try{
     display.innerHTML += `=${eval(display.textContent)}`
+    } catch(e){display.innerHTML=`WTF?`}
 })
 
 
